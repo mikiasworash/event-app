1a72a9e3bb292c2177e0d20b4b0877a69e8df28a
mikias-worash <mikias.worash.a@gmail.com>
2023-01-02 13:15:42 +0300

    Loading data & paths for dynamic pages

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,22 +1,19 @@
-import { useRouter } from "next/router";
-import { Fragment } from "react";
-import { getEventById } from "../../dummy-data";
-import EventSummary from "../../components/event-detail/event-summary";
-import EventLogistics from "../../components/event-detail/event-logistics";
-import EventContent from "../../components/event-detail/event-content";
-import ErrorAlert from "../../components/ui/error-alert";
+import { Fragment } from 'react'
+import { getEventById, getFeaturedEvents } from '../../helpers/api-util'
+import EventSummary from '../../components/event-detail/event-summary'
+import EventLogistics from '../../components/event-detail/event-logistics'
+import EventContent from '../../components/event-detail/event-content'
+import ErrorAlert from '../../components/ui/error-alert'
 
-export default function EventDetailPage() {
-  const router = useRouter();
-  const eventId = router.query.eventId;
-  const event = getEventById(eventId);
+export default function EventDetailPage(props) {
+  const event = props.event
 
   if (!event) {
     return (
-      <ErrorAlert>
-        <p>No event found!</p>
-      </ErrorAlert>
-    );
+      <div className="center">
+        <p>Loading...</p>
+      </div>
+    )
   }
 
   return (
@@ -32,5 +29,26 @@ export default function EventDetailPage() {
         <p>{event.description}</p>
       </EventContent>
     </Fragment>
-  );
+  )
+}
+
+export async function getStaticProps(context) {
+  const eventId = context.params.eventId
+  const event = await getEventById(eventId)
+  return {
+    props: {
+      event: event,
+    },
+    revalidate: 60,
+  }
+}
+
+export async function getStaticPaths() {
+  const events = await getFeaturedEvents()
+
+  const paths = events.map((event) => ({ params: { eventId: event.id } }))
+  return {
+    paths: paths,
+    fallback: true,
+  }
 }
